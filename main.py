f31d27643f451e758d83ae1e16568cd36b3d2bf0
yqooopy <yqoaopy@gmail.com>
2024-09-23 22:53:07 +0800

    Update main.py
    移除測試內容

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 from crawler import PTTCrawler
 from database import SQLiteDataSaver
+import time
 
 
 
-crawler = PTTCrawler('Gossiping')
-# save data to database
+crawler = PTTCrawler('Lifeismoney')
 saver = SQLiteDataSaver('database.db')
 
 # auto crawl and save data to database every 5 sec
-import time
 while True:
     data = crawler.crawl()
     for data in data:
         saver.save_dict(data)
-        # print data
-        # print(saver.query_data("SELECT * FROM data"))
     print("Waiting for 5 sec...")
     time.sleep(5)
 
